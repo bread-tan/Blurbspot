260e7cf92af22a85d2a925a24f66bac2fbadaff2
Archit Shukla <architshukla2@gmail.com>
2013-11-18 18:19:41 +0530

    Restriction imposed: user can like a band only once

--- like.php
+++ like.php
@@ -12,6 +12,15 @@
 
 	require_once("dbAccess.php");
 	mysqli_select_db($con, 'blurbspot') or die(mysqli_error($con));
+
+	$query = mysqli_query($con, "select * from userlikes where bandname='$bandname' and username='".$_SESSION['username']."'");
+	$data = mysqli_fetch_array($query);
+	if(!empty($data))
+	{
+		echo "btn btn-warning|<span class='glyphicon glyphicon-warning-sign'> </span> <strong>Opps! </strong> You already like this band.";
+		return;
+	}
+
 	$query = mysqli_query($con, "select * from likes where bandname='$bandname'");
 	$data = mysqli_fetch_array($query);
 	
@@ -23,6 +32,9 @@
 	{
 		mysqli_query($con, "update likes set numberOfLikes=numberOfLikes+1 where bandname='$bandname'");
 	}
+
+	mysqli_query($con, "insert into userlikes values('".$_SESSION['username']."','$bandname')");
+
 	mysqli_close($con);
 	echo "success";
 	return;
