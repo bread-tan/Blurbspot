5b2294ad814fecc461cf5781edef73db893696c2
Archit Shukla <architshukla2@gmail.com>
2013-11-22 22:31:31 +0530

    Integrated tweets as a mashup
    Signed-off-by: Archit Shukla <architshukla2@gmail.com>

--- tweets.php
+++ tweets.php
@@ -10,7 +10,7 @@ require_once('twitter_library/twitteroauth.php'); // Get it from: https://github
  
 // Use the data from http://dev.twitter.com/apps to fill out this info
 // notice the slight name difference in the last two items)
- $consumer_key = 'gwAWD1g2nqdL43JE5SYe8Q';
+ 	$consumer_key = 'gwAWD1g2nqdL43JE5SYe8Q';
 	$consumer_secret = 'MYpnBZS2iLEKZSkmvxbFxWOX67m98QpGAx6z6LvM';
 	$user_token = '2198271439-HZ6Ps7zeJzHT3IrnGyhR7KSbeV76nvz8Alnv5gZ'; //access token
 	$user_secret = '8nWXAPHeiKuOL37xoOpczWzouEunTk3pwD8wwwYEWiItn';
@@ -20,16 +20,28 @@ $connection = new TwitterOAuth($consumer_key,$consumer_secret,$user_token,$user_
 // set up parameters to pass
 
  
-if (isset($_GET['count'])) {
-	$count = strip_tags($_GET['count']);
+if (isset($_POST['count'])) {
+	$count = strip_tags($_POST['count']);
 }
  
-if (isset($_GET['screen_name'])) {
-	$screen_name = strip_tags($_GET['screen_name']);
+if (isset($_POST['screen_name'])) {
+	$screen_name = strip_tags($_POST['screen_name']);
 }
- 
-$tweets = $connection->get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name='.$screen_name.'&count='.$count);
-//print_r($tweets);
+
+$handle = fopen("config/config.txt", "r");
+$contents = fread($handle, filesize("config/config.txt"));
+$configurations = explode("\r\n", $contents);
+foreach($configurations as $config)
+{
+	$parts = explode("=", $config);
+	if($parts[0] == "python")
+		$pythonSource = $parts[1];
+}
+
+$new_screen_name = rtrim(shell_exec($pythonSource."python scripts/twitterusercrawler.py $screen_name"));
+
+$tweets = $connection->get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name='.$new_screen_name.'&count='.$count);
+// print_r($tweets);
 echo '<label>Name: '.$tweets[0]->user->name.'<label><br>';
 echo '<label>Profile Picture</label><br>';
 echo '<img src='.$tweets[0]->user->profile_image_url_https.'><br>';
